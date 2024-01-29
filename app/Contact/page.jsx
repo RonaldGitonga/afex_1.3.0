1ba2e710a66ff1dec867084304ec65de1c7c5bcb
RonaldGitonga <rgatuiku@gmail.com>
2024-01-29 16:27:57 +0300

    modified:   app/Contact/page.jsx

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -41,7 +41,7 @@ function Contact() {
         )
         .then(()=>{
           setLoading(false);
-          alert('Thank you.I will get back to you as soon as possible');
+          alert('Thank you.We will get back to you as soon as possible');
             setFirstName('')
             setLastName('')
             setemail('')
